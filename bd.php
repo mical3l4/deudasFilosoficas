afd1b56e3974ae3cb45a35edf2d6862d0c0dad1b
JimePy <jimenalopezpy@gmail.com>
2025-04-01 14:51:06 -0400

    Tercer commit

---
+++ bd.php
@@ -0,0 +1,38 @@
+base: u178928053_deudasf
+usuario: u178928053_jimena
+contraseña: =T2NspU#r6I
+
+<?php
+$conexion= new mysqli("localhost", "u178928053_jimena", "2NspU#r6I", "u178928053_deudasf")
+
+$sql = "create table usuario (
+    id int primary key not null auto_increment,
+    nombre varchar(230),
+    apellido varchar(250),
+    usuario varchar(250),
+    contrasenha text,
+    correo text
+);
+";
+
+$conexion->query(query: $sql);
+
+$sql = "create table inicio_sesion (
+    id INT auto_increment primary key not null,
+    correo text,
+    contrasenha varchar(255) not null
+    );
+";
+
+$conexion->query(query: $sql);
+
+$sql = "create table usuario (
+    id INT auto_increment primary key not null,
+    titulo varchar(300), 
+    descripcion text, 
+    fecha date
+);
+";
+
+$conexion->query(query: $sql);
+
